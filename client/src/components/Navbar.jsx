186f77737e0ff4c3b03db27b6a324f889f40563d
linuxnile <linuxnile@gmail.com>
2024-12-17 23:48:02 +0000

    Install all dependencies for client and server

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <AppBar>
+      <Toolbar>
+        <Typography variant="h6">Mern-Auth</Typography>
+        <Button sx={{ color: "inherit" }}>Login</Button>
+        <Button sx={{ color: "inherit" }}>Signup</Button>
+        <Button sx={{ color: "inherit" }}>Logout</Button>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Navbar;
