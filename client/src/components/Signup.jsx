219a8ce856e515ffc20273afd449739aa8eb0566
linuxnile <linuxnile@gmail.com>
2024-12-18 15:59:18 +0000

    Implement working of Login

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Grid2, Paper, TextField, Typography } from "@mui/material";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
   const heading = { fontSize: "2.5rem" };
@@ -11,6 +13,29 @@ const Signup = () => {
   };
   const row = { display: "flex", margin: "2rem" };
 
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = (e) => {
+    console.log(e);
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:3001/signup", { name, email, password })
+      .then((result) => {
+        if (result.status == 201) {
+          navigate("/login");
+          window.alert("User created successfully. Please login");
+        }
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
+  };
+
   return (
     <Grid2 align="center">
       <Paper
@@ -21,22 +46,40 @@ const Signup = () => {
         }}
       >
         <Typography style={heading}>Signup</Typography>
-        <form>
+        <form onSubmit={handleSignup}>
           <TextField
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            name="name"
+            required
             style={row}
             label="Enter your name"
             type="name"
           ></TextField>
+
           <TextField
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            name="email"
+            required
             style={row}
             label="Enter your email"
             type="email"
           ></TextField>
+
           <TextField
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            name="password"
+            required
             style={row}
             label="Enter password"
             type="password"
           ></TextField>
+
           <Button variant="contained" type="submit">
             Signup
           </Button>
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const dotenv = require("dotenv");
+const UserModel = require("./models/User");
 
 dotenv.config();
 app.use(cors());
@@ -17,6 +18,27 @@ mongoose
   .catch((error) => {
     console.log("Database not connected!", error.message);
   });
+
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const checkAlreadyExist = await UserModel.findOne({ email });
+
+    if (checkAlreadyExist) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new UserModel({ name, email, password: hashedPassword });
+    const savedUser = await newUser.save();
+
+    res
+      .status(201)
+      .json({ message: "User created successfully", data: savedUser });
+  } catch (error) {
+    res.status(500).json({ message: "Somethng went wrong" });
+  }
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
