7f66e91b1ac3631bc4f55b5885e52dbe431128c1
linuxnile <linuxnile@gmail.com>
2024-12-18 02:50:31 +0000

    Add basic design for Signup and Login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { Button, Grid2, Paper, TextField, Typography } from "@mui/material";
 
 const Login = () => {
-  return <div>Login</div>;
+  const heading = { fontSize: "2.5rem" };
+  const paperStyle = {
+    padding: "2rem",
+    margin: "100px auto",
+    borderRadius: "1rem",
+    boxShadow: "5px 5px 5px",
+  };
+  const row = { display: "flex", margin: "2rem" };
+
+  return (
+    <Grid2 align="center">
+      <Paper
+        style={paperStyle}
+        sx={{
+          width: { xs: "80", sm: "50vw", md: "40vw", lg: "30vw", xl: "20vw" },
+          height: "60vh",
+        }}
+      >
+        <Typography style={heading}>Login</Typography>
+        <form>
+          <TextField
+            style={row}
+            label="Enter your email"
+            type="email"
+          ></TextField>
+          <TextField
+            style={row}
+            label="Enter password"
+            type="password"
+          ></TextField>
+          <Button variant="contained" type="submit">
+            Login
+          </Button>
+        </form>
+      </Paper>
+    </Grid2>
+  );
 };
 
 export default Login;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,36 @@
 import React from "react";
 import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const buttonStyle = { marginLeft: "20px" };
   return (
     <AppBar>
       <Toolbar>
-        <Typography variant="h6">Mern-Auth</Typography>
-        <Button sx={{ color: "inherit" }}>Login</Button>
-        <Button sx={{ color: "inherit" }}>Signup</Button>
-        <Button sx={{ color: "inherit" }}>Logout</Button>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          Mern-Auth
+        </Typography>
+        <Button variant="contained" color="error" component={Link} to="/login">
+          Login
+        </Button>
+        <Button
+          style={buttonStyle}
+          variant="contained"
+          color="success"
+          component={Link}
+          to="/signup"
+        >
+          Signup
+        </Button>
+        <Button
+          style={buttonStyle}
+          variant="contained"
+          color="error"
+          component={Link}
+          to="/logout"
+        >
+          Logout
+        </Button>
       </Toolbar>
     </AppBar>
   );
